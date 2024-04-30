366557c1cc880296ca18786f9a4d18ba7af60042
qambarov-f <qemberov.f1949@gmail.com>
2024-04-30 11:27:52 +0400

    i added files

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+var name = prompt("Please Write Your Name");
+var surname = prompt("Please Write Your Surname");
+var year = prompt("Please Write Your Birth Year");
+var currentYear = 2024;
+
+var age = currentYear - year;
+
+alert( "Dear " + name + " your age is " + age);
+
+console.log("Dear " + name + " your age is " + age);
